d9b4fdd4f49e4be9498bdd90ae6494b733e032f9
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2026-06-15 22:42:33 +0530

    fast api 02

--- fastapi-02-templates/main.py
+++ fastapi-02-templates/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -31,6 +31,9 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
 
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:    
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
